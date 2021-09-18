1fcd540fdd11476f35bb2bf9577d948fb9de1a92
Wooseong Yang/양우성 <dntjddid1@gmail.com>
2021-09-18 16:20:12 +0900

    add pointer event block

--- src/_components/Loader.js
+++ src/_components/Loader.js
@@ -1,18 +1,31 @@
 export default class Loader {
   constructor($target) {
     const loader = document.createElement('div')
-    $target.innerHTML = ''
-    loader.innerText = 'Loading...'
-    loader.className = 'loader'
     this.loader = loader
-    $target.appendChild(loader)
-
+    this.$target = $target
     this.render()
   }
 
   closeLoader() {
     this.loader.remove()
+    this.allowMouseEvent()
   }
 
-  render() {}
+  blockMouseEvent() {
+    const album = document.querySelector('.album')
+    album.classList.add('not-active')
+  }
+
+  allowMouseEvent() {
+    const album = document.querySelector('.album')
+    album.classList.remove('not-active')
+  }
+
+  render() {
+    this.blockMouseEvent()
+    this.$target.innerHTML = ''
+    this.loader.innerText = 'Loading...'
+    this.loader.className = 'loader'
+    this.$target.appendChild(this.loader)
+  }
 }
